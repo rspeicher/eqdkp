8ef7c8d0062825131358293fc49eec03acca8dff
dazzac <christopher.darroch@gmail.com>
2007-10-06 06:15:28 +0000

    Removed switch(mode) from the index page (doesn't really serve any apparent purpose anymore)
    git-svn-id: https://eqdkp.googlecode.com/svn/branches/inputsec/@71 0a1ec8ee-f233-0410-a8d7-37675f9b97f8

--- index.php
+++ index.php
@@ -15,24 +15,15 @@ define('EQDKP_INC', true);
 $eqdkp_root_path = './';
 include_once($eqdkp_root_path . 'common.php');
 
-$mode = ( isset($_GET['mode']) ) ? $_GET['mode'] : 'redirect';
-
-switch ( $mode )
+// Short and sweet
+if ( isset($eqdkp->config['start_page']) )
 {
-    case 'redirect':
-        // Fall through
-    default:
-        if ( isset($eqdkp->config['start_page']) )
-        {
-            $start_page = preg_replace('#\?' . URI_SESSION . '=([A-Za-z0-9]{32})?#', $SID, $eqdkp->config['start_page']);
-            redirect($start_page);
-        }
-        else
-        {
-            redirect('viewnews.php' . $SID);
-        }
-        
-        break;
+	$start_page = preg_replace('#\?' . URI_SESSION . '=([A-Za-z0-9]{32})?#', $SID, $eqdkp->config['start_page']);
+	redirect($start_page);
+}
+else
+{
+	redirect('viewnews.php' . $SID);
 }
 
 // Thassit

0679ba494fa4876de03133bcb2bd487bb38e3796
Robert Speicher <rspeicher@gmail.com>
2007-10-19 22:06:52 +0000

    Apply the numeric index fix in build_query to UPDATE types as well.
    git-svn-id: https://eqdkp.googlecode.com/svn/branches/inputsec/@168 0a1ec8ee-f233-0410-a8d7-37675f9b97f8

--- dbal/mysql.php
+++ dbal/mysql.php
@@ -242,6 +242,13 @@ class SQL_DB
         {
             foreach ( $array as $field => $value )
             {
+                // Hack to prevent assigning $array directly from a fetch_record call
+                // injecting number-based indices into the built query
+                if ( is_numeric($field) )
+                {
+                    continue;
+                }
+                
                 if ( is_null($value) )
                 {
                     $values[] = "$field = NULL";

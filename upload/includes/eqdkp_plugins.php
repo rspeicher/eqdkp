77bfc60de72c5c425806d66a19fd50f492abf05a
Robert Speicher <rspeicher@gmail.com>
2007-10-26 06:14:16 +0000

    Use valid_folder() in one place I noticed just glancing at this file.
    git-svn-id: https://eqdkp.googlecode.com/svn/branches/inputsec/@250 0a1ec8ee-f233-0410-a8d7-37675f9b97f8

--- upload/includes/eqdkp_plugins.php
+++ upload/includes/eqdkp_plugins.php
@@ -250,13 +250,7 @@ class EQdkp_Plugin_Manager
             while ( $d_plugin_code = @readdir($dir) )
             {
                 $cwd = $eqdkp_root_path . 'plugins/' . $d_plugin_code;
-                if (     (!is_file($cwd)) && 
-             (!is_link($cwd)) && 
-            ($d_plugin_code != '.') && 
-            ($d_plugin_code != '..') && 
-            ($d_plugin_code != 'CVS') && 
-            ($d_plugin_code != '.svn') &&
-            (substr($d_plugin_code, 0, 1) != '_') )
+                if ( valid_folder($cwd) && substr($d_plugin_code, 0, 1) != '_' )
                 {
                     // If $d_plugin_code is in our array of registered codes,
                     // continue with the next iteration of the while loop

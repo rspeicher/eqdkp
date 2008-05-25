1e2ecccabd861c263f10bcd39b2295d355c702e4
Robert Speicher <rspeicher@gmail.com>
2008-05-25 02:42:00 +0000

    array_diff_key is only available in PHP5. Update arrayfuncs.php to show an example of how we can get around this using purely PHP4 functions. See Issue 18.
    git-svn-id: https://eqdkp.googlecode.com/svn/branches/inputsec/@556 0a1ec8ee-f233-0410-a8d7-37675f9b97f8

--- tests/arrayfuncs.php
+++ tests/arrayfuncs.php
@@ -3,4 +3,12 @@ $array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
 $array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
 
 var_dump(array_diff_key($array1, $array2));
+
+$diff_key = array();
+$diffs = array_diff(array_keys($array1), array_keys($array2));
+foreach ( $diffs as $key )
+{
+    $diff_key[$key] = $array1[$key];
+}
+var_dump($diff_key);
 ?>
